188b0670c76ca21435cc5e6f9513c689b8e8e634
Nitikorn Chumnankul <nitikornchumnankul@gmail.com>
2022-02-21 23:16:23 +0700

    add components

--- eye-pacs/src/App.js
+++ eye-pacs/src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
-import UpdateEye from './conponents/form/UpdateEye';
-import Login from './conponents/login/Login';
+import "./App.css";
+import UpdateEye from "./conponents/form/UpdateEye";
+import ImageCard from "./conponents/image-card/ImageCard";
+import Login from "./conponents/login/Login";
+import TablesEyes from "./conponents/tables/TablesEyes";
 
 function App() {
   return (
     <>
-    {/* <UpdateEye></UpdateEye> */}
-    
-    <Login></Login>
+      {/* <UpdateEye></UpdateEye> */}
+      <Login></Login>
     </>
   );
 }
